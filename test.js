ef2424f35c47015e6e8f7bffc306036aa973e013
Kyle L. Jensen <kljensen@gmail.com>
2016-01-18 15:33:34 -0500

    Test for invalid user

--- test.js
+++ test.js
@@ -10,6 +10,13 @@ function makeid(length){
     return text;
 }
 
+function makeUser(){
+  return {
+    email: makeid(10) + '@yale.edu',
+    password: makeid(10)
+  }
+}
+
 casper.test.begin('Todo app authentication', 3, function suite(test) {
   casper.start(base_url, function() {
     test.assertTitle("CPSC113 Todo", "title was as expected");
@@ -17,15 +24,13 @@ casper.test.begin('Todo app authentication', 3, function suite(test) {
     test.assertExists(loginFormPath, "login form is found");
 
     // User that should not exist
-    var user = {
-      password: makeid(10),
-      email: makeid(5) + '@yale.edu'
-    };
+    var user = makeUser();
     this.fill(loginFormPath, {
       email: user.email,
       password: user.password
     }, true);
   });
+
   casper.then(function(){
       test.assertTextExists('Invalid email address', 'notices invalid user');
   })
