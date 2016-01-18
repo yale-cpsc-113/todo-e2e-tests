920ba579942b69cbbc604ef51dc3e84727d2ba60
Kyle L. Jensen <kljensen@gmail.com>
2016-01-18 15:13:54 -0500

    Pass in base_url via command line

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-  casper.start("http://www.google.fr/", function() {
+var base_url = casper.cli.options['base_url'];
+
+casper.test.begin('Todo app authentication', 5, function suite(test) {
+  casper.start(base_url, function() {
     test.assertTitle("Google", "google homepage title is the one expected");
     test.assertExists('form[action="/search"]', "main form is found");
     this.fill('form[action="/search"]', {
